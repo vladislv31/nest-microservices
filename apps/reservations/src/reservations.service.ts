1873e2668dc62670fffc29353ed3a1ada5386218
vladislv31 <vladislv.k31@gmail.com>
2023-09-30 11:41:48 +0300

    reservations crud

--- apps/reservations/src/reservations.service.ts
+++ apps/reservations/src/reservations.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ReservationsRepository } from './reservations.repository';
 
 @Injectable()
 export class ReservationsService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly reservantionsRepository: ReservationsRepository,
+  ) {}
+
+  create(createReservationDto: CreateReservationDto) {
+    return this.reservantionsRepository.create({
+      ...createReservationDto,
+      timestamp: new Date(),
+      userId: '123',
+    });
+  }
+
+  findAll() {
+    return this.reservantionsRepository.find({});
+  }
+
+  findOne(_id: string) {
+    return this.reservantionsRepository.findOne({ _id });
+  }
+
+  update(_id: string, updateReservationDto: UpdateReservationDto) {
+    return this.reservantionsRepository.findOneAndUpdate(
+      { _id },
+      { $set: updateReservationDto },
+    );
+  }
+
+  remove(_id: string) {
+    return this.reservantionsRepository.findOneAndDelete({ _id });
   }
 }
